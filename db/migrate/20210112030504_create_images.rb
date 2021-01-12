eed6a0fec4552c2e4380aed2d049ccd5e5e081ab
dimontejulia <dimontejulia@gmail.com>
2021-01-11 22:09:21 -0500

    migrated model to db

--- db/migrate/20210112030504_create_images.rb
+++ db/migrate/20210112030504_create_images.rb
@@ -1,9 +1,9 @@
 class CreateImages < ActiveRecord::Migration
   def change
     create_table :images do |t|
-      t.text, :name
-      t.text, :description
-      t.text :img_url
+      t.string :name
+      t.string :description
+      t.string :img_url
 
       t.timestamps null: false
     end
